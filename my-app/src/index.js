63a73028cdc9c5bc1b20502101c87379b09b6c2c
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-10-19 20:30:48 -0500

    Class vs Functional components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello world.</h1>;
-  }
+function Hello() {
+  return <h1>Hello world.</h1>;
 }
+
+// class Hello extends React.Component {
+//  render() {
+//    return <h1>Hello world.</h1>;
+//  }
+// }
 
 const el = <Hello />; 
 ReactDOM.render(
   el, 
   document.getElementById('root')
 );
-
